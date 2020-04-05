651b98e5e0ae259c57203c088f08d8e84edf6612
Lucas Wang <luwang@linkedin.com>
2020-04-05 15:35:19 -0700

    Adding rocksdb version 6.7.3

--- cmake/projects/rocksdb/hunter.cmake
+++ cmake/projects/rocksdb/hunter.cmake
@@ -28,6 +28,12 @@ hunter_add_version(
     URL "https://github.com/facebook/rocksdb/archive/v5.14.2.tar.gz"
     SHA1 "376bc537e8c716111214274755f5fe4026e5a354")
 
+hunter_add_version(
+    PACKAGE_NAME rocksdb
+    VERSION 6.7.3
+    URL "https://github.com/facebook/rocksdb/archive/v6.7.3.tar.gz"
+    SHA1 "00601fdfb860d93646436a94bb92fc1db2de2e00")
+
 
 if(MSVC)
     hunter_cmake_args(rocksdb CMAKE_ARGS
